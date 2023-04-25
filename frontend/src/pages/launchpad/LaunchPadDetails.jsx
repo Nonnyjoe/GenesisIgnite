08e0ae353c28d58e39235e191161fb8b598ff13e
Bill <wyze97@gmail.com>
2023-04-25 12:25:55 +0100

    buttons

--- frontend/src/pages/launchpad/LaunchPadDetails.jsx
+++ frontend/src/pages/launchpad/LaunchPadDetails.jsx
@@ -20,10 +20,9 @@ import styles from "../../styles/LaunchPads.module.css";
 import Image from "next/image";
 import igniteNft from "../../images/dodge.png";
 
-
 const LaunchPadDetails = (props) => {
   const { address } = useAccount();
-const { contractAddress } = props;
+  const { contractAddress } = props;
   const DropdownOptions2 = [
     {
       id: 0,
@@ -53,7 +52,6 @@ const { contractAddress } = props;
   const [LaunchPadToken, setLaunchPadToken] = useState();
   const [priceIncrease, setPriceIncrease] = useState();
 
-
   /// FETCH THE CONTRACT TOKEN NAME AND SYMBOL
   const {
     data: BuyersData,
@@ -69,9 +67,8 @@ const { contractAddress } = props;
     },
   });
 
-
   /// SET THE PRICE INCREASE:
- useContractRead({
+  useContractRead({
     address: contractAddress,
     abi: LPABI,
     functionName: "PercentagePriceIncrease",
@@ -80,24 +77,23 @@ const { contractAddress } = props;
     },
   });
 
-
-    useContractRead({
+  useContractRead({
     address: contractAddress,
     abi: LPABI,
     functionName: "viewLaunchPadToken",
     onSuccess(data) {
-      setLaunchPadToken((data).toString());
+      setLaunchPadToken(data.toString());
       console.log(data);
     },
   });
 
-    useContractRead({
+  useContractRead({
     address: LaunchPadFacoryAddr(),
     abi: LPFactoryABI,
     functionName: "returnCid",
-    args:[LaunchPadToken??"0x00"],
+    args: [LaunchPadToken ?? "0x00"],
     onSuccess(data) {
-      setCid((data).toString());
+      setCid(data.toString());
       console.log(data);
     },
   });
@@ -138,7 +134,6 @@ const { contractAddress } = props;
       },
     });
 
-
   /// MAIN INTEGRATION TO PARTICIPATE IN LAUNCHPAD
   const {
     data: participate,
@@ -161,7 +156,6 @@ const { contractAddress } = props;
       },
     });
 
-
   /// WITHDRAW LAUNCHPAD REWARD
   const {
     data: withdraw,
@@ -182,8 +176,6 @@ const { contractAddress } = props;
         console.log("Error: ", error);
       },
     });
-
-
 
   // function to read contract details from blockchain
   const {
@@ -288,25 +280,23 @@ const { contractAddress } = props;
     return epochTime;
   };
 
-
   function handleSubmit(e) {
     e.preventDefault();
     console.log("user Allowance" + userAllowance);
-    if(epochTime2() > LaunchPadDeadline) {
-        getWithdraw?.();
+    if (epochTime2() > LaunchPadDeadline) {
+      getWithdraw?.();
     } else {
       if (userAllowance < Amount * 10 ** 18) {
-          getAlawee?.();
+        getAlawee?.();
       } else {
-          getParticipate?.();
+        getParticipate?.();
       }
     }
-  
   }
   function handleSubmit2b() {
     getParticipate?.();
   }
-  
+
   const epochTime = () => {
     const now = new Date();
     const epochTime = Math.floor(now.getTime() / 1000);
@@ -316,186 +306,204 @@ const { contractAddress } = props;
       return true;
     }
   };
-  
-  const launchStatusClassName = epochTime() ? styles.launchongoing : styles.launchended;
+
+  const launchStatusClassName = epochTime()
+    ? styles.launchongoing
+    : styles.launchended;
 
   return (
     <div className={styles.LaunchpadWhite1}>
-    <div className={styles.LaunchpadSecondWhite}>
-      <div className={styles.Banner}></div>
-      <div className={styles.flexpage}>
-        <div className={styles.dynamiclaunch}>
-          <div className={`${styles.dynamicheader} flex-row-reverse flex gap-6 w-[100%] p-0 m-0 ${styles.uppercase}`}>
-            <div className="w-[75%]">
-            <div className={styles.flex1}>
-              <p className={`${styles.launchpadname} font-pop`}>{LaunchPadData.name}</p>
-            </div>
-            <div className="flex justify-between flex-row">
-            <p className={`${styles.launchpadsymbol} font-pop`}>{LaunchPadData.symbol}</p>
-            <p className={`${launchStatusClassName} font-pop`}>{Lstatus()}</p>
-            </div>
-          </div>
-            <div className="">
-                {cid ? (
-                <div className={styles.tokenLogo}>
-                  <img
-                    src={`https://${cid}.ipfs.w3s.link/tokenLogo`}
-                    alt="Dummy image"
-                    width="100%"
-                    height="100%"
-                  />
+      <div className={styles.LaunchpadSecondWhite}>
+        <div className={styles.Banner}></div>
+        <div className={styles.flexpage}>
+          <div className={styles.dynamiclaunch}>
+            <div
+              className={`${styles.dynamicheader} flex-row-reverse flex gap-6 w-[100%] p-0 m-0 ${styles.uppercase}`}
+            >
+              <div className="w-[75%]">
+                <div className={styles.flex1}>
+                  <p className={`${styles.launchpadname} font-pop`}>
+                    {LaunchPadData.name}
+                  </p>
                 </div>
-                  ) : (
-                <div className={styles.tokenLogo}>
-                  <Image src={igniteNft} />
-
+                <div className="flex justify-between flex-row">
+                  <p className={`${styles.launchpadsymbol} font-pop`}>
+                    {LaunchPadData.symbol}
+                  </p>
+                  <p className={`${launchStatusClassName} font-pop`}>
+                    {Lstatus()}
+                  </p>
                 </div>
-              )}    
-            
-            </div>
-          </div>
-          <div className="flex flex-col gap-6 h-[100%]">
-            <div className={`${styles.generaldetails} font-pop text-center justify-center align-middle flex mt-6`}>
-              <div>
-                <h5 class="mt-6 font-pop">
-                  CONTRIBUTORS:{" "}
-                  <span className={styles.launcpdetailed}>
-                    {noOfLaunchPadContributors}
-                  </span>{" "}
-                </h5>
               </div>
-              <div>
-                <h5 class="mt-4 font-pop">
-                  Total Supply:{" "}
-                  <span className={styles.launcpdetailed}>
-                    {Math.floor(LaunchPadTotalSupply / 10 ** 18)} {LaunchPadData.symbol}{" "}
-                  </span>
-                </h5>
-              </div>
-              <div>
-                <h5 class="mt-4 font-pop">
-                  GIT RAISED : {" "}
-                  <span className={styles.launcpdetailed}>
-                    {Math.floor(genesRaisedFromLaunchPad / 10 ** 18)} GIT
-                  </span>
-                </h5>
-              </div>
-            </div>
-
-          <div className={`${styles.moreDetails}`}>
-            <div className="mt-10">
-              <h5 class="mt-4 font-pop">
-                GIT depositd by user:{" "}
-                <p>{genesDepositedByUser / 10 ** 18} GIT</p>
-              </h5>
-            </div>
-          <div className="">
-            <div className="">
-              <h5 class="mt-4 font-pop">Token presale budget: </h5>
-              <p>
-                {Math.floor(presaleTokenBalance / 10 ** 18)} {LaunchPadData.symbol}
-              </p>
-            </div>
-
-            <div className="">
-              <h5 class="mt-4 font-pop">
-               Price increase after Launchpad:{" "}
-              </h5>
-              <p>{`${priceIncrease} %`}</p>
-            </div>
-          </div>
               <div className="">
-              <h5 class="mt-4 font-pop">
-                Launchpad contract address:{" "}
-              </h5>
-              <p>{contractAddress}</p>
+                {cid ? (
+                  <div className={styles.tokenLogo}>
+                    <img
+                      src={`https://${cid}.ipfs.w3s.link/tokenLogo`}
+                      alt="Dummy image"
+                      width="100%"
+                      height="100%"
+                    />
+                  </div>
+                ) : (
+                  <div className={styles.tokenLogo}>
+                    <Image src={igniteNft} />
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-col gap-6 h-[100%]">
+              <div
+                className={`${styles.generaldetails} font-pop text-center justify-center align-middle flex mt-6`}
+              >
+                <div>
+                  <h5 class="mt-6 font-pop">
+                    CONTRIBUTORS:{" "}
+                    <span className={styles.launcpdetailed}>
+                      {noOfLaunchPadContributors}
+                    </span>{" "}
+                  </h5>
+                </div>
+                <div>
+                  <h5 class="mt-4 font-pop">
+                    Total Supply:{" "}
+                    <span className={styles.launcpdetailed}>
+                      {Math.floor(LaunchPadTotalSupply / 10 ** 18)}{" "}
+                      {LaunchPadData.symbol}{" "}
+                    </span>
+                  </h5>
+                </div>
+                <div>
+                  <h5 class="mt-4 font-pop">
+                    GIT RAISED :{" "}
+                    <span className={styles.launcpdetailed}>
+                      {Math.floor(genesRaisedFromLaunchPad / 10 ** 18)} GIT
+                    </span>
+                  </h5>
+                </div>
+              </div>
+
+              <div className={`${styles.moreDetails}`}>
+                <div className="mt-10">
+                  <h5 class="mt-4 font-pop">
+                    GIT depositd by user:{" "}
+                    <p>{genesDepositedByUser / 10 ** 18} GIT</p>
+                  </h5>
+                </div>
+                <div className="">
+                  <div className="">
+                    <h5 class="mt-4 font-pop">Token presale budget: </h5>
+                    <p>
+                      {Math.floor(presaleTokenBalance / 10 ** 18)}{" "}
+                      {LaunchPadData.symbol}
+                    </p>
+                  </div>
+
+                  <div className="">
+                    <h5 class="mt-4 font-pop">
+                      Price increase after Launchpad:{" "}
+                    </h5>
+                    <p>{`${priceIncrease} %`}</p>
+                  </div>
+                </div>
+                <div className="">
+                  <h5 class="mt-4 font-pop">Launchpad contract address: </h5>
+                  <p>{contractAddress}</p>
+                </div>
+              </div>
             </div>
           </div>
-          </div>
-
-        </div>
-        <div className={styles.leftside}>
-          <div className={`${styles.launchendtime} font-pop mb-6`}>
-            <h5 class="">LAUNCHPAD END TIME: </h5>
-            <p className="font-pop">{LPEndDate?.toLocaleString()}</p>
-          </div>
-          <form onSubmit={handleSubmit}>
-            <div className={`${styles.participate} mb-6`}>
-              <h5 className={`${styles.participationheader} font-pop`}>PARTICIPATE</h5>
-              <p className={`${styles.participationparagraph} text-base`}>
-                {" "}
-                <span className="uppercase">Requirement : </span> To participate, you need GIT Token{" "}
-              </p>
-
-              <div className={`${styles.pl2} mt-9 font-pop w-[100%]`}>
-              
-                <p className="mb-7">
-                  Your GIT BALANCE: {UserBalance / 10 ** 18} GIT
+          <div className={styles.leftside}>
+            <div className={`${styles.launchendtime} font-pop mb-6`}>
+              <h5 class="">LAUNCHPAD END TIME: </h5>
+              <p className="font-pop">{LPEndDate?.toLocaleString()}</p>
+            </div>
+            <form onSubmit={handleSubmit}>
+              <div className={`${styles.participate} mb-6`}>
+                <h5 className={`${styles.participationheader} font-pop`}>
+                  PARTICIPATE
+                </h5>
+                <p className={`${styles.participationparagraph} text-base`}>
+                  {" "}
+                  <span className="uppercase">Requirement : </span> To
+                  participate, you need GIT Token{" "}
                 </p>
-                
-                <input
-                  id="string"
-                  type="Number"
-                  placeholder="0.00"
-                  className={styles.amountbar}
-                  onChange={(e) => {
-                    setAmount(Number(e.target.value));
-                    console.log(Amount);
-                  }}
-                />
-                <br />
-              <div className="flex flex-row gap-5 ml-[-1rem]">
-                { epochTime2() > LaunchPadDeadline ?  
-                <button
-                  type="submit"
-                  onMouseEnter={handleHover}
-                  onMouseLeave={handleMouseLeave}
-                  className={`${styles.launchpadbtn} font-pop text-sm`}
-                  disabled={
-                    withdrawalLoading ||
-                    alaweeLoading ||
-                    loadingAlaweeWaitData ||
-                    participateLoading ||
-                    loadingParticipateWaitData
-                  }
-                >
-                  {loadingWithdrawWaitData || withdrawalLoading ? ".....Processing" : "Withdraw Reward"}
-                </button> : 
-                 <button
-                  type="submit"
-                  onMouseEnter={handleHover}
-                  onMouseLeave={handleMouseLeave}
-                  className={`${styles.launchpadbtn} font-pop text-sm`}
-                  disabled={
-                    alaweeLoading ||
-                    loadingAlaweeWaitData ||
-                    participateLoading ||
-                    loadingParticipateWaitData
-                  }
-                >
-                  {epochTime2() > LaunchPadDeadline
-                    ? "Launchpad Ended"
-                    : userAllowance < Amount * 10 ** 18
-                    ? "APPROVE"
-                    : hovered
-                    ? " Launchpad Ended"
-                    : alaweeLoading ||
-                      loadingAlaweeWaitData ||
-                      participateLoading ||
-                      loadingParticipateWaitData
-                    ? "LOADING...."
-                    : "PARTICIPATE"}
-                </button>   
-                                }
-               
+
+                <div className={`${styles.pl2} mt-9 font-pop w-[100%]`}>
+                  <p className="mb-7">
+                    Your GIT BALANCE: {UserBalance / 10 ** 18} GIT
+                  </p>
+
+                  <input
+                    id="string"
+                    type="Number"
+                    placeholder="0.00"
+                    className={styles.amountbar}
+                    onChange={(e) => {
+                      setAmount(Number(e.target.value));
+                      console.log(Amount);
+                    }}
+                  />
+                  <br />
+                  <div className="flex flex-row gap-5 ml-[-1rem]">
+                    {epochTime2() > LaunchPadDeadline ? (
+                      <button
+                        type="submit"
+                        onMouseEnter={handleHover}
+                        onMouseLeave={handleMouseLeave}
+                        className={`${styles.launchpadbtn} font-pop text-sm`}
+                        disabled={
+                          withdrawalLoading ||
+                          alaweeLoading ||
+                          loadingAlaweeWaitData ||
+                          participateLoading ||
+                          loadingParticipateWaitData
+                        }
+                      >
+                        {loadingWithdrawWaitData || withdrawalLoading
+                          ? ".....Processing"
+                          : "Withdraw Reward"}
+                      </button>
+                    ) : (
+                      <button
+                        type="submit"
+                        onMouseEnter={handleHover}
+                        onMouseLeave={handleMouseLeave}
+                        className={`${styles.launchpadbtn} font-pop text-sm`}
+                        disabled={
+                          alaweeLoading ||
+                          loadingAlaweeWaitData ||
+                          participateLoading ||
+                          loadingParticipateWaitData
+                        }
+                      >
+                        {epochTime2() > LaunchPadDeadline
+                          ? "Launchpad Ended"
+                          : userAllowance < Amount * 10 ** 18
+                          ? "APPROVE"
+                          : hovered
+                          ? " Launchpad Ended"
+                          : alaweeLoading ||
+                            loadingAlaweeWaitData ||
+                            participateLoading ||
+                            loadingParticipateWaitData
+                          ? "LOADING...."
+                          : "PARTICIPATE"}
+                      </button>
+                    )}
+                  </div>
+                </div>
               </div>
-              </div>
-            </div>
-          </form>
+            </form>
+          </div>
+        </div>
+        <div className="flex justify-center items-center">
+          <button className={`${styles.launchpadbtn2} font-pop text-sm`}>
+            Check {LaunchPadData.name} DAO proposals
+          </button>
         </div>
       </div>
     </div>
-  </div>
   );
 };
 
